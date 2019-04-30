fd441f7034320918d45279dc46e1af4431df58fc
Ihor Shkurko <ishkurko@gmail.com>
2019-04-30 15:45:19 +0300

    Remove characteristic
    Signed-off-by: Ihor Shkurko <ishkurko@gmail.com>

--- src/main/java/com/icthh/xm/tmf/ms/qualification/web/rest/PromotionQualificationDelegate.java
+++ src/main/java/com/icthh/xm/tmf/ms/qualification/web/rest/PromotionQualificationDelegate.java
@@ -4,13 +4,14 @@ import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
 import com.icthh.xm.tmf.ms.qualification.lep.keyresolver.ProfileKeyResolver;
 import com.icthh.xm.tmf.ms.qualification.web.api.PromotionQualificationApiDelegate;
-import com.icthh.xm.tmf.ms.qualification.web.api.model.Characteristic;
 import com.icthh.xm.tmf.ms.qualification.web.api.model.PromotionQualification;
 import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @LepService(group = "service", name = "default")
 public class PromotionQualificationDelegate implements PromotionQualificationApiDelegate {
@@ -21,8 +22,10 @@ public class PromotionQualificationDelegate implements PromotionQualificationApi
     @Override
     public ResponseEntity<PromotionQualification> promotionQualificationFind(String profile,
                                                                              String relatedPartyId,
-                                                                             String channelId,
-                                                                             Characteristic characteristic) {
+                                                                             String channelId) {
+        if (getRequest().isPresent()) {
+            log.info("Native request {}", getRequest().get().getNativeRequest());
+        }
         return ResponseEntity.ok(new PromotionQualification());
     }
 }
